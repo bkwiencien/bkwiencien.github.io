b380e27d8d8d58ca54992daaa31291bd4278595d
Bob Kwiencien <bkwiencien@gmail.com>
2018-07-01 17:52:27 -0400

    breakthrouh

--- CoinFlips/assets/javascript/coinflips.js
+++ CoinFlips/assets/javascript/coinflips.js
@@ -1,5 +1,6 @@
 function initialize() {
 	var a = 11
+	console.log('in init')
 }
 function flip(){
 	var nn = Math.random()*1000
@@ -9,4 +10,26 @@ function flip(){
 	} else {
 		return('T')
 	}
+}
+function getParms() {
+	var seq = $('#chsequence').val()
+	var numb = $('#numbtrials').val()
+	if (seq == 'hh') {
+		hh(parseInt(numb))
+	} else {
+		ht(parseInt(numb))
+	}
+}
+function hh(numbTrials) {
+   var aresults = []
+   var state = 0
+   while (j=0;numbTrials-1;j++){
+   	  while (state != 3){
+   	  	state = 3
+   	  }
+   }
+}
+function ht(numbTrials) {
+   var aresults = []
+   var state = 0;
 }
