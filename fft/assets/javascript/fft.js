9888690d1889365f09817bfe3ec216835b3eea60
Bob Kwiencien <bkwiencien@gmail.com>
2018-07-09 13:23:36 -0400

    plot

--- fft/assets/javascript/fft.js
+++ fft/assets/javascript/fft.js
@@ -16,11 +16,33 @@ var sineWave = {
 		var e = document.getElementById("choosefreq");
 		var strFreq = e.options[e.selectedIndex].text;
 		this.freq=parseInt(strFreq)
+		var layout = {
+             title: "Sine plot " + this.freq,
+        xaxis: {
+        type: "linear",
+        range: [
+        0,
+        2*this.pi],
+        autorange: true
+      },
+      yaxis: {
+      type: "linear",
+      range: [-1,1],
+      autorange: true
+      },
+      height: 598,
+      width: 1080,
+      autosize: true,
+      showlegend: false
+     }
 		for (var j=0;j<100;j++) {
 			this.xAxis.push(theta)
 			this.yAxis.push(Math.sin(theta))
 			theta = theta + .01*2*this.pi
 		}
+		var dd = [this.xAxis,this.yAxis]
+		Plotly.newPlot('sineplot',dd,layout);
+
 	},
 }
 function initialize() {
