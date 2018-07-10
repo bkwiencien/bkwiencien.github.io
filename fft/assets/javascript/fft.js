e53b397f973c6849a9f21fe0810f6d2a44324c47
Bob Kwiencien <bkwiencien@gmail.com>
2018-07-10 12:31:27 -0400

    minor chenges

--- fft/assets/javascript/fft.js
+++ fft/assets/javascript/fft.js
@@ -13,35 +13,42 @@ var sineWave = {
 	},
 	generateData: function() {
 		var theta = 0.0
+		var dd = []
 		var e = document.getElementById("choosefreq");
 		var strFreq = e.options[e.selectedIndex].text;
 		this.freq=parseInt(strFreq)
 		var layout = {
-             title: "Sine plot " + this.freq,
+	    x: [],
+	    y: [],		
+        title: "Sine plot " + this.freq,
         xaxis: {
-        type: "linear",
-        range: [
-        0,
-        2*this.pi],
-        autorange: true
-      },
-      yaxis: {
-      type: "linear",
-      range: [-1,1],
-      autorange: true
-      },
+        type: "scatter",
+        showgrid: true,
+        range: [0,2*this.pi],
+        autorange: false,
+        },
+        yaxis: {
+        type: "scatter",
+        showgrid: true,
+        range: [-1,1],
+        autorange: false,
+        },
       height: 598,
       width: 1080,
       autosize: true,
-      showlegend: false
+      showlegend: true,
      }
-		for (var j=0;j<100;j++) {
+		for (var j=0;j<150;j++) {
 			this.xAxis.push(theta)
-			this.yAxis.push(Math.sin(theta))
-			theta = theta + .01*2*this.pi
+			this.yAxis.push(Math.sin(2*theta*Math.PI/this.freq))
+			console.log(Math.sin(2*theta*Math.PI/this.freq))
+			theta = theta + .048
 		}
-		var dd = [this.xAxis,this.yAxis]
-		Plotly.newPlot('sineplot',dd,layout);
+		dd = [this.xAxis,this.yAxis]
+		Plotly.purge('sineplot');
+		console.log("after purge " + dd)
+		Plotly.plot('sineplot',dd,layout)
+	    $("body").css("cursor", "default");
 
 	},
 }
